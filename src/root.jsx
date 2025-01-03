2c0f44c60eb52b89c5261d826f1eb2efd66066a2
catta1eya <p.kubica1605@gmail.com>
2025-01-03 01:24:14 +0100

    Cleanups

--- src/root.jsx
+++ src/root.jsx
@@ -1,14 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import "./index.css";
-import StarRating from "./StarRating";
-// import App from './App.jsx'
+import "./index.css";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={20} />
-    <StarRating maxRating={10} />
-    <StarRating />
+    <App />
   </StrictMode>
 );
